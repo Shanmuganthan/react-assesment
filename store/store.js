a8ded66958845ddf6422a4e23435c95a8fd12061
shaninvestera <rsnathan29@gmail.com>
2023-03-24 15:57:17 +0400

    Clean Up

--- store/store.js
+++ store/store.js
@@ -12,7 +12,6 @@ const initalState = {};
 // middleware
 const sagaMiddleware = createSagaMiddleware()
 
-// creating store
 export const store = createStore(
     reducer,
     initalState,
@@ -21,36 +20,9 @@ export const store = createStore(
 
 sagaMiddleware.run(rootSaga);
 
-// assigning store to next wrapper
 const makeStore = () => store;
 
 export const wrapper = createWrapper(makeStore);
 
 
 
-
-// import { applyMiddleware, createStore } from 'redux';
-// import reducer from './reducer';
-// import { createWrapper } from 'next-redux-wrapper';
-// import createSagaMiddleware from 'redux-saga';
-// import rootSaga from './sagaRoot';
-
-
-
-// const bindMiddleware = (middleware) => {
-//     if (process.env.NODE_ENV !== 'production') {
-//         const { composeWithDevTools } = require('redux-devtools-extension');
-//         return composeWithDevTools(applyMiddleware(...middleware));
-//     }
-//     return applyMiddleware(...middleware);
-// };
-
-// export const makeStore = (context) => {
-//     const sagaMiddleware = createSagaMiddleware();
-//     const store = createStore(reducer, applyMiddleware(sagaMiddleware));
-//     sagaMiddleware.run(rootSaga);
-//     return store;
-// };
-
-
-// export const wrapper = createWrapper(makeStore, { debug: true });
